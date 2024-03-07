5f7a7cb1287a65b3013e3dbbdec0c9fc86a56e52
chrls <repuestocharlene@gmail.com>
2024-03-07 10:29:41 +0800

    dsdf

--- login.php
+++ login.php
@@ -18,7 +18,7 @@
                     <input type="password" class="form-control" name="password" id="password" placeholder="Enter password..." required>
                     <label for="password">Password</label>
                 </div>
-                <button type="submit" role="button" class="btn btn-outline-dark btn-lg btn-block">Log In</button>
+                <button id="btnLogIn" value="1" type="submit" role="button" class="btn btn-outline-dark btn-lg btn-block">Log In</button>
 
             </div>
         </form>
@@ -35,3 +35,30 @@
         </a>
     </nav>
 </footer>
+
+<?php	
+	if(isset($_POST['btnLogin'])){
+		$uname=$_POST['username'];
+		$pwd=$_POST['password'];
+		//check tbluseraccount if username is existing
+		$sql ="Select * from tbluseraccount where username='".$uname."'";
+		
+		$result = mysqli_query($connection,$sql);	
+		
+		$count = mysqli_num_rows($result);
+		$row = mysqli_fetch_array($result);
+		
+		if($count== 0){
+			echo "<script language='javascript'>
+						alert('username not existing.');
+				  </script>";
+		}else if($row[3] != $pwd) {
+			echo "<script language='javascript'>
+						alert('Incorrect password');
+				  </script>";
+		}else{
+			$_SESSION['username']=$row[0];
+			header("location: index.php");
+		}
+	}
+?>
--- register.php
+++ register.php
@@ -9,20 +9,25 @@
         <form action="<?php ?>" method="post">
             <div class="formsch">
                 <div class="form-floating mb-3">
-                    <input type="text" class="form-control" name="firstName" id="firstName" placeholder="First Name" required>
-                    <label for="firstName">First Name</label>
+                    <input type="text" class="form-control" name="firstname" id="firstname" placeholder="First Name" required>
+                    <label for="firstname">First Name</label>
                 </div>
 
                 <div class="form-floating mb-3">
-                    <input type="text" class="form-control" name="lastName" id="lastName" placeholder="Last Name" required>
-                    <label for="lastName">Last Name</label>
+                    <input type="text" class="form-control" name="lastname" id="lastname" placeholder="Last Name" required>
+                    <label for="lastname">Last Name</label>
                 </div>
 
                 <div class="form-floating mb-3">
                     <input type="date" class="form-control" id="birthdate" name="birthdate">
                     <label for="birthdate">Birthday:</label>
                 </div>
-                    
+                
+                <div class="form-floating mb-3">
+                    <input type="email" class="form-control" name="email" id="email" placeholder="Email" required>
+                    <label for="email">Email</label>
+                </div>
+
                 <div class="form-floating mb-3"> 
                     <input type="text" class="form-control" name="username" id="username" placeholder="Username" required>
                     <label for="username">Username</label>
@@ -39,7 +44,7 @@
                 </div>
         
             
-                <button id="btnRegister" type="submit" role="button" class="btn btn-outline-warning btn-lg btn-block">Sign in</button>
+                <button id="btnRegister" type="submit" role="button" class="btn btn-outline-warning btn-lg btn-block" value="1">Sign in</button>
             </div>
         </form>
 
@@ -63,15 +68,15 @@
 		//for tbluserprofile
 		$fname=$_POST['firstName'];		
 		$lname=$_POST['lastName'];
-		
+		$bdate=$_POST['birthdate'];
 		
 		//for tbluseraccount
-		$email=$_POST['txtemail'];		
-		$uname=$_POST['txtpassword'];
-		$pword=$_POST['txtpassword'];
+		$email=$_POST['email'];		
+		$uname=$_POST['username'];
+		$pword=$_POST['password'];
 		
 		//save data to tbluserprofile			
-		$sql1 ="Insert into tbluserprofile(firstname,lastname) values('".$fname."','".$lname."')";
+		$sql1 ="Insert into tbluserprofile(firstname,lastname,birthdate) values('".$fname."','".$lname."', '".$bdate."')";
 		mysqli_query($connection,$sql1);
 		
 		//Check tbluseraccount if username is already existing. Save info if false. Prompt msg if true.
@@ -79,7 +84,7 @@
 		$result = mysqli_query($connection,$sql2);
 		$row = mysqli_num_rows($result);
 		if($row == 0){
-			$sql ="Insert into tbluseraccount(emailadd,username,password) values('".$email."','".$uname."','".$pword."')";
+			$sql ="Insert into tbluseraccount(email_add,username,password) values('".$email."','".$uname."','".$pword."')";
 			mysqli_query($connection,$sql);
 			echo "<script language='javascript'>
 						alert('New record saved.');
@@ -89,8 +94,6 @@
 						alert('Username already existing');
 				  </script>";
 		}
-			
-		
 	}
 		
 
